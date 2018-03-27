253ab1c167ab0036746a0a0d3c452dd6c3b008dd
masalism <mantasmasalis@gmail.com>
2018-03-27 21:27:33 +0300

    su papildymu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,11 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 
 public class Controller {
@@ -18,11 +20,19 @@ public class Controller {
     public void tryMe(ActionEvent event) {
         Random rnd = new Random();
         int rndNumber = rnd.nextInt(10) + 1;
-        int guessNumber = Integer.valueOf(inputNumber.getText());
-        if (rndNumber == guessNumber) {
-            answerLabel.setText("Jus atspejote skaiciu, tai buvo " + rndNumber);
-        } else {
-            answerLabel.setText("Jus neatspejote, jusu spejimas " + guessNumber + " , o buvo " + rndNumber);
+        int guessNumber;
+        try {
+            guessNumber = Integer.valueOf(inputNumber.getText());
+            inputNumber.setText(""); //isvalo viska
+            if (rndNumber == guessNumber) {
+                answerLabel.setText("Jus atspejote skaiciu, tai buvo " + rndNumber);
+            } else {
+                answerLabel.setText("Jus neatspejote, jusu spejimas " + guessNumber + " , o buvo " + rndNumber);
+            }
+        }catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Ka cia darai? Vesk skaiciu!!");
+            alert.show();
         }
     }
 }
